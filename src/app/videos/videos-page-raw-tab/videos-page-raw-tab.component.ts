cf4cd8119cebfb26453be7cb240c0b91d74b0024
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2021-09-04 23:34:59 +0300

    Page navigation on videos page is now reflected in address bar.

--- src/app/videos/videos-page-raw-tab/videos-page-raw-tab.component.ts
+++ src/app/videos/videos-page-raw-tab/videos-page-raw-tab.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { VideoDataI } from 'src/app/shared/interfaces/ServiceDataInterfaces';
 import { ServerService } from 'src/app/shared/services/server.service';
 import { VideoI } from 'src/app/shared/interfaces/videos-interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-videos-page-raw-tab',
@@ -12,46 +13,73 @@ import { VideoI } from 'src/app/shared/interfaces/videos-interface';
 export class VideosPageRawTabComponent implements OnInit {
 
   videos: VideoI[] = [];
-  subscription!: Subscription;
+  serverSubscription!: Subscription;
+  queryParamSubscription!: Subscription;
   numOfPages: number = 0;
   itemsPerPage: number = 6;
   currentPage = 1;
-  dataHandler!: {next: Function};
 
-  constructor(private server: ServerService) { }
+  constructor(
+    private server: ServerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.subscription = this.server.getVideos()
-      .subscribe(<any>this.getDataHandler())
+    this.queryParamSubscription = this.route.queryParams
+      .subscribe(<any>this.getQueryParamsSubscriber());
+  }
+
+  getQueryParamsSubscriber(){
+    let next = (params: any) => {
+      let page = 1;
+
+      if(params.hasOwnProperty("page")){
+        page = parseInt(params.page);
+        if(isNaN(page) || page <= 0){
+          page = 1;
+        }
+      }
+
+      if(this.serverSubscription){
+        this.serverSubscription.unsubscribe();
+      }
+      this.serverSubscription = this.server.getVideos(page)
+        .subscribe(<any>this.getdataSubscriber())
+    }
+
+    return {next}
   }
 
   pageChange(pageNum: number){
-    if(pageNum > this.numOfPages)
+    if(pageNum > this.numOfPages){
       pageNum = this.numOfPages;
+    }
 
-    this.currentPage = pageNum;
-    
-    this.subscription.unsubscribe();
-    this.subscription = this.server.getVideos(pageNum)
-      .subscribe(<any>this.getDataHandler());
+    this.router.navigate([],{
+      queryParams: {
+        page : pageNum
+      },
+      relativeTo: this.route
+    })
   }
 
-  getDataHandler(){
-    if(!this.dataHandler){
-      this.dataHandler = {
+  getdataSubscriber(){
+    return {
         next: (response: VideoDataI) => {
           this.numOfPages = response.pages;
           this.videos = response.data;
         }
-      }
     }
-
-    return this.dataHandler;
   }
   
   
   ngOnDestroy(){
-    this.subscription.unsubscribe();
+    this.queryParamSubscription.unsubscribe();
+
+    if(this.serverSubscription){
+      this.serverSubscription.unsubscribe();
+    }
   }
 
 }
